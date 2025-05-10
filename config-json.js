7deb632c0ac20cdc6d25d1664f1b274765ca3437
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-05-11 05:15:39 +0800

    Update config-json.js

--- config-json.js
+++ config-json.js
@@ -3,6 +3,7 @@ let CONFIG = {
   publicURLShorten: `https://ppt.cc/f6Ddzx`,
   thumbnailBorderColor: '8A95A5',
   titleAppend: '[YT+]',
+  title: 'action-RSS-UT-Weekly-202504',
   idAppend: false,
 
   maxDownloadItemPerFeed: 5,
